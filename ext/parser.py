59d54f001a2a8bd4a0f90f7bf8557cf49f0d7cf6
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2021-04-01 20:07:08 +0200

    Create message parser and colour convertor
    ext/parser Version: 0.0.1
    -- Initial Commit --

---
+++ ext/parser.py
@@ -0,0 +1,42 @@
+# File: ext/parser
+# Author: Theo Technicguy
+# Interpreter: Python 3.9
+# Ext: py
+# -----------------------
+
+from . import colours
+
+# ---------- START Program Constants ----------
+__author__ = "Theo Technicguy"
+__version__ = "0.0.1"
+# ---------- END Program Constants ----------
+
+
+def colour(name: str) -> str:
+    """Convert standard string colour to hexadeimal string.
+
+    pre: Standard colours library in same package
+    args:
+        name: str - Standard colour name.
+    return: Hexadeimal string representation of the colour.
+    """
+    return eval(f"colours.{name}")
+
+
+def message(
+    template: str, data: dict, start_flag: str = "$", end_flag: str = "$"
+) -> str:
+    """Insert data values into template.
+
+    pre: --
+    args:
+        template: str - Template to fill.
+        data: dict - key representing template variables.
+    opts:
+        start_flag: str (`$`) - Start flag.
+        end_flag: str (`$`) - End flag.
+    return: Filled template
+    """
+    for key, value in data.items():
+        template = template.replace(start_flag + str(key) + end_flag, str(value))
+    return template
