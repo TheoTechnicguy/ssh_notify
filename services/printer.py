15bcd0b17dbcc50b8adc82df9f072a49d9453109
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2021-04-10 14:04:14 +0200

    Officialize printer service file
    services/printer Version: 1.0.0
    - Add desctiption string to print satements
    - Remove type printing
    - Add file header and program constants
    - Add type casts

--- services/printer.py
+++ services/printer.py
@@ -1,4 +1,18 @@
-def notify(data, message):
-    print(data)
-    print(type(data))
-    print(message)
+# File: services/printer
+# Author: Theo Technicguy
+# Interpreter: Python 3.9
+# Ext: py
+# Licensed under MIT. See LICENSE for details.
+# -----------------------
+
+
+# ---------- START Program Constants ----------
+__author__ = "Theo Technicguy"
+__version__ = "1.0.0"
+# ---------- END Program Constants ----------
+
+
+def notify(data: dict, message: dict):
+    """Print notifiction data."""
+    print("data", data)
+    print("message", message)
